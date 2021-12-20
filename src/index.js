931eb6a8801278d749d7ec681a272b0926fcd252
Nam Eungyeong <nek1717@naver.com>
2021-12-21 00:32:16 +0900

    리덕스 개발자도구 install

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import App from "./App";
 // import "./exercise";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자도구
 
 import rootReducer from "./modules";
 
-// 스토어 생성
-const store = createStore(rootReducer);
+// 스토어 생성, 리덕스 개발자 도구 활성화
+const store = createStore(rootReducer, composeWithDevTools());
 console.log(store.getState());
 
 // 리덕스 스토어 전역 설정
